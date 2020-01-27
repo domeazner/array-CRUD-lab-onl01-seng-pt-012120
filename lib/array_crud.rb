b3077916cb853ac8fcfaf20659e5a726d86ffa48
Dominc Carolan <dominiccarolan@gmail.com>
2020-01-27 19:01:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["cat", "dog", "bird", "elephant", "zebra"]
+  create_an_array = ["cat", "dog", "bird", "elephant", "zebra"]
 end
 
 def add_element_to_end_of_array(array, element)
